1fd192c07bd5a119db41fd328c8e38607cc57d00
Nomsies <thopek007@gmail.com>
2023-07-12 12:13:08 +0700

    fix bug level

--- NextLevel.gd
+++ NextLevel.gd
@@ -4,4 +4,5 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
+			print(str(int(get_tree().current_scene.name) + 1))
 			get_tree().change_scene("res://Level/Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
